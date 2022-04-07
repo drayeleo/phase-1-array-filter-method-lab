4ba29a34977d7730272dae3d06d9a5f2049e4b03
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-04-07 15:51:19 -0600

    commit tree 667431b876966948db6f8b53cbdaa0d84dc8e714

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+//Code your solution here
+
+function findMatching(arr, targetName) {
+    return arr.filter(element => element.toUpperCase() === targetName.toUpperCase());
+}
+
+// function findMatching(arr, targetName) {
+//     return arr.filter(function (element) {
+//         return element.toUpperCase() === targetName.toUpperCase();
+//     })
+// }
+
+function  fuzzyMatch(arr, targetName) {
+    return arr.filter(function(element) {
+        return element.toUpperCase()[0] === targetName.toUpperCase()[0];
+    })
+}
+
+function matchName (arr, targetName) {
+    return arr.filter (element => element.name === targetName);
+}
